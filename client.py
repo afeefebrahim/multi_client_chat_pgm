ae879ea19943a06c6adb7ed77bc52b837488760f
afeefebrahim <afeefebrahim@gmail.com>
2015-11-17 13:23:53 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,23 @@
+import sys
+import select
+import socket
+port = 4020
+host = "127.0.0.1"
+fd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+fd.connect((host,port))
+inp = [fd,sys.stdin]
+while True:
+       inputready = select.select(inp,[],[])
+       for s in inputready[0]:
+           if s == fd:
+                data = fd.recv(1024)
+                if not data:
+                    print "disconnect from chat server"
+                    sys.exit()
+                else: 
+                    print "recived msg",data
+           elif s == sys.stdin:
+                msg = raw_input(">>>")
+                print msg
+                fd.send(msg)
+fd.close()  
---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import select
+import sys
+size = 1024
+port = 4020
+host = "127.0.0.1"
+fd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+fd.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+fd.bind((host,port))
+fd.listen(5)
+sock_list =[fd,sys.stdin]
+while True:
+       input_ready = select.select(sock_list,[],[])
+       for s in input_ready[0]:
+           if s == fd:
+              client,addr = fd.accept()
+              sock_list.append(client)
+           
+           elif s == sys.stdin:
+              msg = raw_input(">>")
+              for sock in sock_list:
+                    if sock != fd and sock != sys.stdin:
+                       sock.send(msg)
+           
+           else:
+             
+              data = s.recv(size)
+              print data
+              for sock in sock_list:
+                      if sock != s and sock != fd and sock != sys.stdin: 
+                            sock.send(data)
+s.close() 
+     
